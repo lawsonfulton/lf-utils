39b8eeed9e445bc2fd51bd6d49bfc41c5be1cc07
zero-impact <lawsonfulton@gmail.com>
2019-01-03 10:56:10 -0500

    making it work with header only

--- cpp/lf_utils.cpp
+++ cpp/lf_utils.cpp
@@ -1,6 +1,8 @@
 #include "lf_utils.h"
 
-bool lf::str::endswith (const std::string &fullString, const std::string &ending) {
+using std::string;
+
+bool lf::str::endswith (const string &fullString, const string &ending) {
 	if (fullString.length() >= ending.length()) {
 		return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
 	} else {
@@ -9,17 +11,33 @@ bool lf::str::endswith (const std::string &fullString, const std::string &ending
 }
 
 
-std::string remove_extension(const std::string & filename) {
+string lf::path::remove_extension(const string & filename) {
 	const size_t p = filename.find_last_of('.');
-	return p > 0 && p != std::string::npos ? filename.substr(0, p) : filename;
+	return p > 0 && p != string::npos ? filename.substr(0, p) : filename;
 }
 
-std::string lf::path::base_name(const std::string & path, bool keep_extension) {
-	std::string basename = path.substr(path.find_last_of("/\\") + 1);
+string lf::path::base_name(const string & path, bool keep_extension) {
+	string basename = path.substr(path.find_last_of("/\\") + 1);
 
 	if(keep_extension) {
 		return basename;
 	} else {
 		return remove_extension(basename);
+	}
+}
+
+string lf::path::join(const string& p1, const string& p2) {
+	char sep = '/';
+	string tmp = p1;
+
+	#ifdef _WIN32
+	sep = '\\';
+	#endif
+
+	if (p1[p1.length()] != sep) { // Need to add a
+		tmp += sep;               // path separator
+		return(tmp + p2);
+	} else {
+		return(p1 + p2);
 	}
 }
--- cpp/lf_utils.h
+++ cpp/lf_utils.h
@@ -5,16 +5,18 @@
 
 namespace lf {
 	namespace str {
-		bool endswith (const std::string &fullString, const std::string &ending);
+		inline bool endswith (const std::string &fullString, const std::string &ending);
 	}
 
 	namespace path {
-		std::string base_name(const std::string &path, bool keep_extension = true);
+		inline std::string remove_extension(const std::string &filename);
+		inline std::string base_name(const std::string &path, bool keep_extension = true);
+		inline std::string join(const std::string &p1, const std::string &p2);
 	}
 }
 
 #define IS_HEADER_ONLY
-#ifdef IS_HEADER_ONLY
+#ifdef IS_HEADER_ONLY	
 #include "lf_utils.cpp"
 #endif
 
